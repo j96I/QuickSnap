04243e9314aca21162cfd4af476679d0283b866c
Jordan Ibrahim <j.ibrahim@me.com>
2016-03-09 12:12:18 +1100

    add player hit with left and right shift, with score decrement for miss hits

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -4,6 +4,8 @@ using CardGames.GameLogic;
 
 namespace CardGames
 {
+
+
     public class SnapGame
     {
         public static void LoadResources()
@@ -17,15 +19,32 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
-		{
-			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
-
+		private static void HandleUserInput(Snap myGame) 
+		{ 
+			//Fetch the next batch of UI interaction  SwinGame.ProcessEvents(); 
+ 			SwinGame.ProcessEvents(); 
+		
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
 				myGame.FlipNextCard ();
 			}
+
+			if (myGame.IsStarted)
+			{
+				if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
+					 SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) 
+				{
+					//TODO: add sound effects 
+				}
+				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) 
+				{
+				myGame.PlayerHit (0); 
+				}
+				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) 
+				{
+					myGame.PlayerHit (1); 
+				}
+			} 
 		}
 
 		/// <summary>
